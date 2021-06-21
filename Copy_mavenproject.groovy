5017bdb18e2e8e7d1b786f30185be0f6b0a53e10
pulilohith <69109777+pulilohith@users.noreply.github.com>
2021-06-21 16:23:10 +0530

    Add files via upload

---
+++ Copy_mavenproject.groovy
@@ -0,0 +1,22 @@
+job('create a maven project using DSL'){
+    description('This is my first DSL project created on' ${new Date()} )
+    logRotator{
+        daysToKeep(2)
+        numToKeep(3)
+    }
+    scm{
+        github('https://github.com/anshulc55/Jenkins_Upgradev3.git','master')
+
+    }
+    triggers{
+        scm('* * * * *')
+    }
+    steps{
+        maven('clean package')
+
+    }
+    publishers{
+        archiveArtifacts('**/*.jar')
+    }
+
+}
